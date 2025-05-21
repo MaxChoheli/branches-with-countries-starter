a6a8674ae9cf3501cde5a7e2650f2015b5b98568
Max Choheli <max.choheli@gmail.com>
2025-05-21 23:34:09 +0300

    made a repo

--- js/country.controller.js
+++ js/country.controller.js
@@ -1,9 +1,27 @@
 'use strict'
 
 function onGetCountryInfo() {
-    console.log('Hi')
+    const elInput = document.getElementById('country-input')
+    const countryName = elInput.value.trim()
+    if (!countryName) return
+
+    axios.get(`https://restcountries.com/v3.1/name/${countryName}`)
+        .then(res => {
+            const country = res.data[0]
+            renderInfo(country)
+        })
+        .catch(() => {
+            document.getElementById('country-output').innerText = 'Country not found'
+        })
 }
 
 function renderInfo(data) {
-    console.log('Rendering...')
+    const output = `
+Name: ${data.name.common}
+Capital: ${data.capital}
+Population: ${data.population.toLocaleString()}
+Area: ${data.area.toLocaleString()} km²
+Region: ${data.region}
+  `
+    document.getElementById('country-output').innerText = output
 }
